0172352b6a0aa2984fc1fbe23b4b2337e03ca682
Satvik Prasad <satvikprasad@gmail.com>
2023-09-20 21:38:52 +1000

    linux

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cl main.c -o main.exe -ID:\lib\raylib\raylib\src D:\lib\raylib\raylib\projects\VS2022\build\raylib\bin\x64\Release.DLL\raylib.lib
+cl main.c -o bin\main.exe -ID:\lib\raylib\raylib\src D:\lib\raylib\raylib\projects\VS2022\build\raylib\bin\x64\Release.DLL\raylib.lib
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gcc main.c -o bin/main -I/home/satvik/lib/raylib/src/ -L /home/satvik/lib/raylib/src/ -lraylib -lm
+
